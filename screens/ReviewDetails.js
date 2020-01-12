3ba69a7d9912f10b5b79aeada4df282acd769759
Imane <imaneelmouradi98@gmail.com>
2020-01-12 21:34:55 +0100

    add: navigation

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+  const pressHandler = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text>Review Details screen</Text>
+      <Button title="Go back to home screen" onPress={pressHandler} />
     </View>
   );
 }
