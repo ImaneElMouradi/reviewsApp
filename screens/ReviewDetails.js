7a44ec934345fee2322224af547d559af9f93063
Imane <imaneelmouradi98@gmail.com>
2020-01-12 21:53:04 +0100

    add: send data frm home to review details

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -4,14 +4,12 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
 export default function ReviewDetails({ navigation }) {
-  const pressHandler = () => {
-    navigation.goBack();
-  };
-
   return (
     <View style={globalStyles.container}>
       <Text>Review Details screen</Text>
-      <Button title="Go back to home screen" onPress={pressHandler} />
+      <Text>{navigation.getParam("title")}</Text>
+      <Text>{navigation.getParam("body")}</Text>
+      <Text>{navigation.getParam("rating")}</Text>
     </View>
   );
 }
