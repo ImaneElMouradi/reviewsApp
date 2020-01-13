35e9d7e5745ab41eb0d7ca654f2fc8970bd849f4
Imane <imaneelmouradi98@gmail.com>
2020-01-13 12:31:05 +0100

    add: rating imgs

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,19 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-import { globalStyles } from "../styles/global";
+import { globalStyles, images } from "../styles/global";
 
 import Card from "../shared/card";
 
 export default function ReviewDetails({ navigation }) {
+  const rating = navigation.getParam("rating");
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>Review Details screen</Text>
         <Text>{navigation.getParam("title")}</Text>
         <Text>{navigation.getParam("body")}</Text>
-        <Text>{navigation.getParam("rating")}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating:</Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 2,
+    borderTopColor: "#eee"
+  }
+});
